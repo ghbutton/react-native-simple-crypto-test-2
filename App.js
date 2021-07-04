3e50ccfb772a44be7de1ddcee71b30920198a162
Gary Button <garyhbutton@gmail.com>
2021-07-03 23:45:32 -0700

    Test with new xcode

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RNSimpleCrypto from "react-native-simple-crypto";
 
 export default function App() {
+  const message = "data to encrypt";
+  const messageArrayBuffer = RNSimpleCrypto.utils.convertUtf8ToArrayBuffer(
+    message
+  );
+  console.log(message);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
